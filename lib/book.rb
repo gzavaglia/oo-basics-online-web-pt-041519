a0cb51a893690afda7780c3709317adccfcca7b0
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-23 13:49:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,19 +1,21 @@
 class Book
+  attr_accessor :author, :genre, :page_count
+  
   def initialize(book_title)
     @title = book_title
   end 
   
-  def title
-    @title
-  end 
+  # def title
+  #   @title
+  # end 
   
-  def author=(author)
-    @author = author
-  end 
+  # def author=(author)
+  #   @author = author
+  # end 
   
-  def author
-    @author
-  end 
+  # def author
+  #   @author
+  # end 
   
 end 
 
